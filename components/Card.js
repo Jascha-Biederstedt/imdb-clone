2a364ab72d09ea3530527c7aa75ea13af6ac7258
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-04-27 18:38:12 +0200

    Finish results section

--- components/Card.js
+++ components/Card.js
@@ -6,7 +6,7 @@ const Card = ({ result }) => {
   const BASE_URL = 'https://image.tmdb.org/t/p/original';
 
   return (
-    <div>
+    <div className="p-3 cursor-pointer hover:text-white active:text-red-400 xl:hover:scale-105 transition-transform duration-200">
       <Image
         layout="responsive"
         src={BASE_URL + result.backdrop_path}
@@ -14,9 +14,9 @@ const Card = ({ result }) => {
         height={100}
       />
 
-      <div>
-        <p className="truncate">{result.overview}</p>
-        <h2>{result.title || result.name}</h2>
+      <div className="p-2">
+        <p className="truncate text-lg">{result.overview}</p>
+        <h2 className="text-lg font-bold">{result.title || result.name}</h2>
         <p className="flex items-center">
           {result.release_date || result.first_air_date}
           <ThumbUpIcon className="h-5 ml-3 mr-1" />

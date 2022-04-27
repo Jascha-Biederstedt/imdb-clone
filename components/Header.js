3c0e1bb260ff3adeb38f16fb8bfd53ff4c01941e
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-04-27 16:40:27 +0200

    Style the header

--- components/Header.js
+++ components/Header.js
@@ -11,8 +11,8 @@ import HeaderIcon from './HeaderIcon';
 
 const Header = () => {
   return (
-    <div>
-      <div>
+    <div className="bg-gray-700 text-gray-200 flex flex-col items-center p-6 select-none sm:flex-row justify-between">
+      <div className="flex">
         <HeaderIcon Icon={HomeIcon} title="HOME" />
         <HeaderIcon Icon={UserIcon} title="ACCOUNT" />
         <HeaderIcon Icon={PhoneIcon} title="CONTACT" />
@@ -23,6 +23,7 @@ const Header = () => {
         src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg"
         width={100}
         height={100}
+        className="cursor-pointer hover:brightness-105"
       />
     </div>
   );
